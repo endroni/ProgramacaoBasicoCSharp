8455f1258c544f15d8cab729b0c73221093b0524
endroni <rubao.rcs@gmail.com>
2020-11-20 16:37:30 -0300

    aula 2 - atribuição de valores

---
+++ ProgramacaoEstruturada/ProgramacaoEstruturada/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProgramacaoEstruturada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int aux;
+            aux = a;
+            a = b;
+            b = aux;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
