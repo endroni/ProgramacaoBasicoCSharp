4230e94134cfdd6336ac22d3ff29e5d73bba08c1
endroni <rubao.rcs@gmail.com>
2020-11-26 07:00:03 -0300

    fun~ção

---
+++ Funcao/Funcao/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Funcao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois números: ");
+            double v1 = double.Parse(Console.ReadLine());
+            double v2 = double.Parse(Console.ReadLine());
+            double res = Multiplicar(v1, v2);
+            Console.WriteLine(res);
+        }
+
+        private static double Multiplicar(double v1, double v2)
+        {
+            double res = v1 * v2;
+            return res;
+        }
+    }
+}
+
+
